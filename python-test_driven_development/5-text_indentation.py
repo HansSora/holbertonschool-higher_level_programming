1b83cd618d274bb9e7012f6a9df697e47b0fb457
HansSora <hansisoraa@gmail.com>
2024-09-13 10:03:14 +0000

    fixes

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,26 +1,11 @@
 #!/usr/bin/python3
-"""Python program to add 2 integers"""
-
-
 def text_indentation(text):
-    """Function to print a square
-
-        Args:
-            text (str): text that is to be printed
-
-        Return:
-            Returns a indented text
-    """
-
-    if not isinstance(text, str):
+    if type(text) is not str:
         raise TypeError("text must be a string")
-
-    i = 0
-    while i < len(text):
-        print(text[i], end="")
-        if text[i] in ['.', ':', '?']:
-            if i != len(text) - 1 and text[i + 1] == " ":
-                i += 1
-            print("\n")
-
-        i += 1
+    begin = 0
+    for i, j in enumerate(text):
+        if j in '.?:':
+            print(text[begin: i + 1].strip() + '\n')
+            begin = i + 1
+    if begin < len(text):
+        print(text[begin:].strip(), end="")
