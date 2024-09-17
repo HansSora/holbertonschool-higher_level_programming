df4cf27abfb5883b3ec2aa5b904640b0dc7fa884
HansSora <hansisoraa@gmail.com>
2024-09-17 11:34:50 +0000

    square1.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A square class that represents an actual square"""
+
+
+class Square:
+    """ An empty square class
+
+        Args:
+            size (int): The size of the square
+
+    """
+
+    def __init__(self, size):
+        self.__size = size
