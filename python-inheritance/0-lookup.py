ede9b6594ebd3fda988174f319100866183a5b5a
HansSora <hansisoraa@gmail.com>
2024-09-23 10:31:37 +0000

    lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A program that returns a list of an objects attributes, variables etc"""
+
+
+def lookup(obj):
+    """A function that looks up variables and stuff about an object
+
+        Return:
+            List containing variables and attributes
+    """
+    return list(obj)
