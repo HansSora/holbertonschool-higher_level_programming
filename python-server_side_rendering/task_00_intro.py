291657babe0714978f205ffa52697eac07428e6e
HansSora <hansisoraa@gmail.com>
2024-11-10 22:58:27 +0100

    task00

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -24,3 +24,6 @@ def generate_invitations(template, attendees):
             personal_template = personal_template.replace('{' + attr + '}', value)
         with open('output_' + str(i + 1) + ".txt", 'w', encoding="utf-8") as f:
             f.write(personal_template)
+
+    
+
